054252231be40453377614300ca08dc4699d7239
Jonathan Matsuda <jonathanmatsuda3@gmail.com>
2021-09-30 10:04:23 -0700

    Completed convertHoursToMinutes exercise

--- prep-javascript-functions/main.js
+++ prep-javascript-functions/main.js
@@ -4,3 +4,10 @@ function addTwoNumbers(num1, num2) {
 
 var addTwoNumbersResult = addTwoNumbers(2, 2);
 console.log('addTwoNumbers Exercise:', addTwoNumbersResult);
+
+function convertHoursToMinutes(hours) {
+  return hours * 60;
+}
+
+var convertHoursToMinutesResult = convertHoursToMinutes(2);
+console.log('convertHoursToMinutes Exercise:', convertHoursToMinutesResult);
